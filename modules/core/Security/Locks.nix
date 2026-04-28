ca3cc5286b0a5a889e045a30c9e4a98624dd9fe7
eliasj <eliasj@Framework>
2026-04-28 15:00:27 -0700

    updated sleep times

---
+++ modules/core/Security/Locks.nix
@@ -0,0 +1,68 @@
+{ config, pkgs, ...}:
+{
+	environment.systemPackages = with pkgs; [
+		swaylock
+		swayidle
+	];
+
+
+  home-manager.users.eliasj = {
+  
+  	home.packages = [
+  	
+  	];
+		services.swayidle =
+		let
+			# Lock command
+			lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+			# TODO: modify "display" function based on your window manager
+			# Sway
+			# display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
+			# Hyprland
+			# display = status: "hyprctl dispatch dpms ${status}";
+			# Niri
+			 display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+		in
+		{
+			enable = true;
+			timeouts = [
+				{
+				  timeout = 500; # in seconds
+				  command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+				}
+				{
+				  timeout = 200;
+				  command = lock;
+				}
+				{
+				  timeout = 160;
+				  command = display "off";
+				  resumeCommand = display "on";
+				}
+				{
+				  timeout = 300;
+				  command = "${pkgs.systemd}/bin/systemctl suspend";
+				}
+			];
+			events = [
+				{
+				  event = "before-sleep";
+				  # adding duplicated entries for the same event may not work
+				  command = (display "off") + "; " + lock;
+				}
+				{
+				  event = "after-resume";
+				  command = display "on";
+				}
+				{
+				  event = "lock";
+				  command = (display "off") + "; " + lock;
+				}
+				{
+				  event = "unlock";
+				  command = display "on";
+				}
+			];
+		};
+	};
+}
