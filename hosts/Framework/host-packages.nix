a2729ec738375930f1f705cc5cd9dc44556432be
eliasj <eliasj@Framework>
2026-04-29 10:12:57 -0700

    .

--- hosts/Framework/host-packages.nix
+++ hosts/Framework/host-packages.nix
@@ -4,6 +4,9 @@
     # Add host-specific packages here
     librewolf	#web browser
     
+    kdePackages.kate	#code editor
+    
+    
     thunderbird	#email client
     
     arduino
@@ -11,10 +14,26 @@
     koreader  #document viewer
 		nautilus  #file manager
 		
-		#kicad	#PCB designer
+		kicad	#PCB designer
+		
+		
+		freecad	#3D design program
+		  gsettings-desktop-schemas	#freecad dependancies
+		  glib
+		  gtk3
+			adwaita-icon-theme
+		
+		
 		
 		signal-desktop	#secure messaging service
 
 		freetube	#FOSS alternitive to youtube
+		heroic
   ];
+  
+  environment.sessionVariables = {	#attempt to fix freecad
+    XDG_DATA_DIRS = [ "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}" ];
+  };
 }
+
+
--- hosts/Framework/variables.nix
+++ hosts/Framework/variables.nix
@@ -52,7 +52,7 @@
   defaultShell = "fish";   # Options: "fish" or "zsh"
 
   # Theming
-  stylixImage = ../../wallpapers/nix-wallpaper-stripes-logo.png;
+  stylixImage = ../../wallpapers/Artimis_II/EarthInTheCapsule.jpg;
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;  # Waybar temporarily disabled
   animChoice = ../../modules/home/hyprland/animations-end4.nix;
 
