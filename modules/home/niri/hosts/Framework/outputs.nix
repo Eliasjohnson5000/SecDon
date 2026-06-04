32da94a4c742543e1847b378a1d9054ef34edad8
Eliasjohnson5000 <eliasjohnson941@gmail.com>
2026-06-04 01:45:45 -0700

    added automatic options for some monitors

--- modules/home/niri/hosts/Framework/outputs.nix
+++ modules/home/niri/hosts/Framework/outputs.nix
@@ -6,7 +6,23 @@
   output "eDP-1" {
     mode "2560x1600@165.000"
     scale 1.2
+    position x=0 y=1100
+  }
+
+  output "Dell Inc. DELL S2340M 293M34CO09KT" {
+    mode "1920x1080@60.000"
+    scale 1
+    position x=2134 y=0
+  }
+    output "DP-2" {
+    mode "1920x1080@60.000"
+    scale 1
     position x=0 y=0
+  }
+    output "DP-4" {
+    mode "1920x1080@60.000"
+    scale 1
+    position x=-2134 y=0
   }
 
   // Add more outputs as needed
