acfb074d4174f3478e0f5346646623dfb476e7ce
Eliasjohnson5000 <eliasjohnson941@gmail.com>
2026-07-25 02:34:26 -0700

    enabled syncthing

--- modules/features/System/syncthing.nix
+++ modules/features/System/syncthing.nix
@@ -2,7 +2,7 @@
 
   flake.nixosModules.syncthing = { pkgs, lib, ... }: {
     services.syncthing = {
-      enable = false;
+      enable = true;
       user = "${userSettings.syncthingName}";
       dataDir = "/home/${userSettings.username}";  # default location for new folders
       configDir = "/home/${userSettings.username}/.config/syncthing";
