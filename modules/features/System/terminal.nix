f24dd6fe04ba471a6c322032077ad377f7c1acaf
Eliasjohnson5000 <eliasjohnson941@gmail.com>
2026-08-05 01:21:35 -0700

    Added shell things

--- modules/features/System/terminal.nix
+++ modules/features/System/terminal.nix
@@ -9,5 +9,33 @@
     environment.systemPackages = [
       pkgs.alacritty
     ];
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+
+
+        # System aliases
+        c = "clear";
+
+        # NixOS specific
+        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+
+        # Directory navigation shortcuts
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        ".3" = "cd ../../..";
+        ".4" = "cd ../../../..";
+        ".5" = "cd ../../../../..";
+
+        
+
+        # Always mkdir with parents
+        mkdir = "mkdir -p";
+      };
+    };
+    users.users.${userSettings.username} = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+    };
   };
 }
